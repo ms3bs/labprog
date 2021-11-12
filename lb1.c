6d08917c2ca8a07255f1e3ef5619c14f5b7fba5a
ms3b <ms3bs@mail.ru>
2021-11-12 13:22:11 +0300

    add input

--- lb1.c
+++ lb1.c
@@ -4,5 +4,19 @@
 #include <locale.h>
 int main(int argc, char* argv[])
 {
+	setlocale(LC_ALL, "Rus");
+	FILE* f;
+	if (argc < 2)
+	{
+		printf("введите\n");
+		printf("1.[option]\n  -l, --lines\n  -c, --bytes\n  -w, --words\n");
+		printf("2.filename");
+		return 0;
+	}
+	if ((f = fopen(argv[2], "r")) == NULL)
+	{
+		printf("такого файла нет");
+		return 0;
+	}
 	return 0;
 }
