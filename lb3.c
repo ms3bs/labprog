d34698a627c9cfb8844116b1f4e3bffd94ea4c2e
ms3b <ms3bs@mail.ru>
2021-11-12 14:36:30 +0300

    start

--- lb3.c
+++ lb3.c
@@ -4,5 +4,13 @@
 #include<math.h>
 int main()
 {
+	FILE* in = fopen("access_log_Jul95.txt", "r");
+	FILE* out = fopen("0.txt", "w");
+	int par, head = 0, tail = 0, m = 0, max = 0, sum = 0, output[2];
+	scanf("%d", &par);
+	int* steck = malloc(sizeof(int) * (par + 2));
+	int* steck2 = malloc(sizeof(int) * (par + 2));
+	int mu[] = { 24 * 60 * 60, 0, 60 * 60, 60, 1 };
+	char strl[300];
 	return 0;
 }
